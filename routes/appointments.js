b67c13ad0e7ec0df17e0fde6bb7fbc590c7d8398
Domenik Töfflinger <domenikt96@yahoo.de>
2020-10-22 18:10:22 +0200

    Update appointments.js

--- routes/appointments.js
+++ routes/appointments.js
@@ -3,15 +3,14 @@ const router = express.Router();
 
 const fetch = require('node-fetch');
 const base64 = require('base-64');
+const moment = require('moment');
 
 const apiUrl = process.env.ACUITY_BASE_URL;
 const userId = process.env.ACUITY_USER_ID;
 const apiKey = process.env.ACUITY_API_KEY;
 
-const max = 1;
+const max = 100;
 const calendarID = '1840022';
-const minDate = new Date().toISOString();
-const maxDate = new Date().toISOString();
 
 /* ===============================================================
    GET /appointments
@@ -24,9 +23,8 @@ const maxDate = new Date().toISOString();
  * @returns {Error}  default - Unexpected error
  */
 router.get('/', (req, res) => {
-  console.log(minDate);
   const url =
-    apiUrl + 'appointments?max=' + max + '&calendarID=' + calendarID + '&minDate=' + minDate + '&maxDate=' + maxDate;
+    apiUrl + 'appointments?max=' + max + '&calendarID=' + calendarID;
   fetch(url, {
     method: 'GET',
     headers: {
@@ -36,12 +34,32 @@ router.get('/', (req, res) => {
   })
     .then((response) => response.json())
     .then((appointments) => {
-      console.log(appointments)
-      if (appointments.length === 0) {
-        res.json({ appointment: 'Es wurde kein aktueller Termin gefunden.' });
-      } else {
-        res.json({ appointment: appointments });
-      }
+      const dates = appointments.map(appointment => {
+        const rObj = {};
+
+        const endTimeHour = appointment.endTime.toString().slice(0, 2);
+        const endTimeMinute = appointment.endTime.toString().slice(3, 5);
+        let endTime = new Date(appointment.datetime).setHours(endTimeHour, endTimeMinute);
+        let endTimeString = new Date(endTime).toString();
+        let endTimeMoment = moment(endTimeString).format();
+
+        rObj['firstName'] = appointment.firstName;
+        rObj['lastName'] = appointment.lastName;
+        rObj['timeStart'] = appointment.datetime.toString();
+        rObj['timeEnd'] = endTimeMoment;
+
+        return rObj;
+      });
+
+      const now = moment();
+
+      const result = dates.find(({ timeStart, timeEnd }) => {
+        const start = new Date(timeStart);
+        const end = new Date(timeEnd);
+        return start <= now && end > now;
+      });
+      
+      res.json(result);
     })
     .catch((error) => {
       throw error;
