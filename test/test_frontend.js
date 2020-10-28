3aa89203011124c1d117964d8bf3864d9f10e417
Domenik Töfflinger <domenikt96@yahoo.de>
2020-10-28 14:46:29 +0100

    Update test_frontend.js

--- test/test_frontend.js
+++ test/test_frontend.js
@@ -2,16 +2,19 @@ const app = require('../index');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
+let async = require('async');
 chai.use(chaiHttp);
 
-describe('Front-end pages', function() {
-  it('landing page exists', (done) => {
-    chai.request(app)
-      .get('/')
-      .end(function (err, res) {
-        res.status.should.be.equal(200);
-        res.should.be.html;
-        done();
-      });
+describe('Front-end pages', () => {
+  describe('index.html', () => {
+    it('landing page exists', (done) => {
+      chai.request(app)
+        .get('/')
+        .end(function (err, res) {
+          res.status.should.be.equal(200);
+          res.should.be.html;
+          done();
+        });
+    });
   });
 });
